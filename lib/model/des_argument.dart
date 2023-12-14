37f1aa7d70c81a40f7e5a6b701d445c01cc8a6fe
HUSAM ALI <114479972+7U5AM@users.noreply.github.com>
2023-12-14 17:53:04 +0300

    Delete lib directory

--- lib/model/des_argument.dart
+++
@@ -1,6 +0,0 @@
-import 'package:ye_airways/model/des_model.dart';
-
-class DestinationArgument {
-  HN_DestinatioModel destinationArg;
-  DestinationArgument(this.destinationArg);
-}
--- lib/shared/components/rsponse.dart
+++
@@ -1,7 +0,0 @@
-import 'package:firebase_auth/firebase_auth.dart';
-
-class AuthResponse {
-  User? user;
-  String messag;
-  AuthResponse({this.user, required this.messag});
-}
